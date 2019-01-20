d13a92d5ea33f833ddfa221403fc02934ddc4cb2
kimseasok <basicblogtalk@gmail.com>
2019-01-20 00:44:09 -0500

    Add routing to the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  template: `
-  <div>
-    <h1 class="display-4">{{pageTitle}}</h1>
-    <pm-products></pm-products>
-  </div>
-  `
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent {
