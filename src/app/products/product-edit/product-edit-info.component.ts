cf7302e679d885679b7594ea74c26e9c09b7deb8
kimseasok <basicblogtalk@gmail.com>
2019-02-02 00:56:30 -0500

    Obtain data to child route

--- src/app/products/product-edit/product-edit-info.component.ts
+++ src/app/products/product-edit/product-edit-info.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ResolvedReflectiveProvider
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
-import { Product } from '../product';
+import { ProductResolved } from '../product';
 
 @Component({
   templateUrl: './product-edit-info.component.html'
 })
 export class ProductEditInfoComponent implements OnInit {
   @ViewChild(NgForm) productForm: NgForm;
+  product: ProductResolved;
 
-  errorMessage: string;
-  product = { id: 1, productName: 'test', productCode: 'test' };
-
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.parent.data.subscribe(data => {
+      this.product = data['product'].product;
+    });
   }
 }
--- src/app/products/product-edit/product-edit-tags.component.ts
+++ src/app/products/product-edit/product-edit-tags.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Product } from '../product';
+import { Product, ProductResolved } from '../product';
 
 @Component({
   templateUrl: './product-edit-tags.component.html'
@@ -9,20 +9,26 @@ import { Product } from '../product';
 export class ProductEditTagsComponent implements OnInit {
   errorMessage: string;
   newTags = '';
-  product = { id: 1, category: 'test', tags: ['test'] };
+  product: ProductResolved;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.parent.data.subscribe(data => {
+      this.product = data['product'].product;
+    });
   }
 
   // Add the defined tags
   addTags(): void {
     if (!this.newTags) {
-      this.errorMessage = 'Enter the search keywords separated by commas and then press Add';
+      this.errorMessage =
+        'Enter the search keywords separated by commas and then press Add';
     } else {
       const tagArray = this.newTags.split(',');
-      this.product.tags = this.product.tags ? this.product.tags.concat(tagArray) : tagArray;
+      this.product.tags = this.product.tags
+        ? this.product.tags.concat(tagArray)
+        : tagArray;
       this.newTags = '';
       this.errorMessage = '';
     }
