2e6988443e77128b4607f3511b07ef9cfb681d9d
kimseasok <basicblogtalk@gmail.com>
2019-02-01 23:48:49 -0500

    Associate resolver with product route

--- src/app/products/product.routes.ts
+++ src/app/products/product.routes.ts
@@ -1,9 +1,18 @@
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductResolverService } from './product-resolver.service';
 
 export const productRoutes = [
   { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'products/:id/edit', component: ProductEditComponent }
+  {
+    path: 'products/:id',
+    component: ProductDetailComponent,
+    resolve: { product: ProductResolverService }
+  },
+  {
+    path: 'products/:id/edit',
+    component: ProductEditComponent,
+    resolve: { product: ProductResolverService }
+  }
 ];
