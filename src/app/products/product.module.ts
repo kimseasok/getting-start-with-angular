cfa665728caacc6a6555ab3717407f8a5cfb7a5e
kimseasok <basicblogtalk@gmail.com>
2019-02-02 00:39:39 -0500

    Add child routes and view

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -7,6 +7,8 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { productRoutes } from './product.routes';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 @NgModule({
   imports: [
@@ -16,7 +18,9 @@ import { productRoutes } from './product.routes';
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
--- src/app/products/product.routes.ts
+++ src/app/products/product.routes.ts
@@ -2,6 +2,8 @@ import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductResolverService } from './product-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 export const productRoutes = [
   { path: 'products', component: ProductListComponent },
@@ -13,6 +15,11 @@ export const productRoutes = [
   {
     path: 'products/:id/edit',
     component: ProductEditComponent,
-    resolve: { product: ProductResolverService }
+    resolve: { product: ProductResolverService },
+    children: [
+      { path: '', redirectTo: 'info', pathMatch: 'full' },
+      { path: 'info', component: ProductEditInfoComponent },
+      { path: 'tags', component: ProductEditTagsComponent }
+    ]
   }
 ];
