68edeb25aff992124dfb08eeded78ba18c677be6
kimseasok <basicblogtalk@gmail.com>
2019-02-07 02:58:55 -0500

    Add runtime validation to the radio

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -16,9 +16,11 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit() {
     this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
       sendCatalog: true
     });
   }
@@ -35,6 +37,8 @@ export class CustomerComponent implements OnInit {
       firstName: 'Jack',
       lastName: 'Harkness',
       email: 'jack@torchwood.com',
+      phone: '',
+      notification: 'email',
       sendCatalog: true
     });
   }
@@ -43,5 +47,16 @@ export class CustomerComponent implements OnInit {
     this.customerForm.patchValue({
       firstName: 'Kimsea'
     });
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+
+    phoneControl.updateValueAndValidity();
   }
 }
